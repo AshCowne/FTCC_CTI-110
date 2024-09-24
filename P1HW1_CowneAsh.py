75e434c5f6f770de3036a880693808172c63a8c7
AshCowne <159182934+AshCowne@users.noreply.github.com>
2024-09-24 14:35:57 -0400

    Add files via upload

---
+++ P1HW1_CowneAsh.py
@@ -0,0 +1,18 @@
+# Ash Cowne
+# 9/24/2025
+# P1HW1
+# Get interger input from the user and preform math caculations.
+
+# Display output to user.
+
+print("----------------Calculationg Exponents----------------");
+print();
+print();
+
+
+# Getting
+
+bass_value_integer = input("Enter and interger as the base value: ")
+exponent_value_integer = input("Enter and interger as the exponent: ")
+
+ 
