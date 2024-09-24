0ea2a7ef3727ee334db63d0369df7b3804177045
AshCowne <159182934+AshCowne@users.noreply.github.com>
2024-09-24 14:21:19 -0400

    First upload

---
+++ P1LAB1_CowneAsh.py
@@ -0,0 +1,16 @@
+# Ash Cowne
+# 9/24/2024
+# P1Lab1
+# Makeing a code to ask for your first name and last name and printing Hello, first name last name! Welcom to CTI-110.
+
+# Get first name from the user.
+
+first_name = input("Enter your first name: ");
+
+# Get last name from the user.
+
+last_name = input("Enter your last name: ");
+
+#Display info to the user.
+
+print("Helo,",first_name,last_name + "! Welcome to CTI-110.");
