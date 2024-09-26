79b3918d28ec2ac6623b7bb646581bf7ca3634fd
AshCowne <159182934+AshCowne@users.noreply.github.com>
2024-09-26 13:04:01 -0400

    Update P1HW1_CowneAsh.py

--- P1HW1_CowneAsh.py
+++ P1HW1_CowneAsh.py
@@ -9,10 +9,11 @@ print("----------------Calculationg Exponents----------------");
 print();
 print();
 
-
-# Getting
-
-bass_value_integer = input("Enter and interger as the base value: ")
+# Getting the integers
+base_value_integer = input("Enter and interger as the base value: ")
 exponent_value_integer = input("Enter and interger as the exponent: ")
+base_^_exponent = base_value_integer ^ exponent_value_integer
+
+print(base_value_integer, " raised to the power of ", exponent_value_integer, "  is ", base_^_exponent, " !!")
 
  
