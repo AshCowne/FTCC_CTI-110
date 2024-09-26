dadb4fadc20ff9099e35aa58ef42106aba7d1625
AshCowne <159182934+AshCowne@users.noreply.github.com>
2024-09-26 13:21:41 -0400

    Update P1HW1_CowneAsh.py

--- P1HW1_CowneAsh.py
+++ P1HW1_CowneAsh.py
@@ -9,11 +9,32 @@ print("----------------Calculationg Exponents----------------");
 print();
 print();
 
-# Getting the integers
-base_value_integer = input("Enter and interger as the base value: ")
-exponent_value_integer = input("Enter and interger as the exponent: ")
-base_^_exponent = base_value_integer ^ exponent_value_integer
+# Getting the integers.
+base_value_integer = int(input("Enter an interger as the base value: "));
+exponent_value_integer = int(input("Enter an interger as the exponent: "));
+# Calculationg the exponents outcome.
+integer_expmemts_out_come = int(base_value_integer **  exponent_value_integer);
 
-print(base_value_integer, " raised to the power of ", exponent_value_integer, "  is ", base_^_exponent, " !!")
+# Showing the outcome of the input.
+print(base_value_integer, " raised to the power of ", exponent_value_integer, "  is ", integer_expmemts_out_come, " !!");
+print();
+print();
 
- 
+
+# Display output to user.
+print("----------------Addition and Subtraction----------------");
+print();
+print();
+
+# Getting integers.
+starting_integer = int(input("Enter a starting integer: "));
+adding_integer = int(input("Enter an interger to add: "));
+subtracting_integer = int(input("Enter an interger to subtract: "));
+# Calculating the outcome.
+integer_add_subract_out_come = int(starting_integer + adding_integer - subtracting_integer );
+print();
+print();
+
+# Shows the user the outcome.
+print(starting_integer, " + ", adding_integer," - ", subtracting_integer, " is equal to ", integer_add_subract_out_come);
+
