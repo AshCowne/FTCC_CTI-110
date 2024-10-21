03af586711cc6ca191a5d880e00408e145bbd01b
AshCowne <159182934+AshCowne@users.noreply.github.com>
2024-10-20 22:30:58 -0400

    Update P2HW2_AshCowne.py

--- P2HW2_AshCowne.py
+++ P2HW2_AshCowne.py
@@ -21,8 +21,8 @@ average = sum(test_grades) / len(test_grades)
 
 #Printing statments.
 print("----------------Results---------------");
-print(f"{'Lowest Grade:':<30} {max(test_grades)}");
-print(f"{'Highest Grade:':<31} {min(test_grades)}");
+print(f"{'Lowest Grade:':<30} {min(test_grades)}");
+print(f"{'Highest Grade:':<31} {max(test_grades)}");
 print(f"{'Sum of  Grade:':<31} {sum(test_grades)}");
 print(f"{'Average:':<34} {round(average,2)}");
 print("---"*23);
