40ba3c162124b30781fae0c6a9ecaaf5aebe2c7c
AshCowne <159182934+AshCowne@users.noreply.github.com>
2024-10-24 14:31:16 -0400

    Update P3HW2_AshCowne.py

--- P3HW2_AshCowne.py
+++ P3HW2_AshCowne.py
@@ -16,7 +16,7 @@ if employee_hours > 40 :
     employee_over_time_pay = employee_hours - 40
 
 # Caculations for how much a employee would get paid for normal hours, overtime hours, and thier gross pay.
-employee_hours = employee_hours * employee_pay_rate
+employee_pay_rate = employee_hours * employee_pay_rate
 employee_over_time_pay = employee_over_time_pay * employee_pay_rate
 employee_gross_pay = employee_hours + employee_over_time_pay
 
@@ -25,6 +25,6 @@ print("-----------------------------------------------------------------"*2);
 print(f"{'Employee name:':<30} {employee_name}");
 print();
 print(f"{'Hours Worked':<31} {'Pay Rate':<31} {'Overtime':<31} {'OverTime Pay':<31} {'RegHour Pay':<31} {'Gross Pay':<31} ");
-print(f"{employee_hours} {'Sum of  Grade:':<31} {employee_pay_rate}");
+print(f"{'employee_hours':<31} {'employee_pay_rate':<31} {'employee_hours':<31} {'employee_over_time_pay':<31} {'employee_hours':<31} {'employee_hours':<31}  {'employee_hours':<31}");
 #print(f"{'Average:':<34} {avg:,.2f}");
 print("----------"*50);
