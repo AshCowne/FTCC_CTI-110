393c856669de442e11983249c8db1576f761d61b
AshCowne <159182934+AshCowne@users.noreply.github.com>
2024-10-24 14:01:43 -0400

    Add files via upload

---
+++ P3HW1_AshCowne.py
@@ -0,0 +1,12 @@
+# Ash cowne
+# 10/24/2024
+# If-else statemts
+
+
+num1 = 10
+num2 = 20
+
+if num1 > num2 :
+    print(f"{num1} is greater than {num2}.");
+else:
+    print(f"{num1} is not greater than {num2}.")
