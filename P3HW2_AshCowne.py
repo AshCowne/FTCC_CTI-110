b31b49611e45ccc2f102a64a361a0c143119c1d7
AshCowne <159182934+AshCowne@users.noreply.github.com>
2024-10-27 23:13:30 -0400

    Update P3HW2_AshCowne.py

--- P3HW2_AshCowne.py
+++ P3HW2_AshCowne.py
@@ -8,12 +8,13 @@
 employee_name = input("Please enter your name: ");
 employee_hours = float(input("Please enter how many hours you've worked this week: "));
 employee_pay_rate = float(input("Please enter your pay rate (Specifically how much you make a hour): "));
+employee_over = 0
 employee_over_time_pay = 0
 employee_gross_pay = 0
 
 # if statment to chech if employee has overtime and caculates the extra hours of overtime.
 if employee_hours > 40 :
-    employee_over_time_pay = employee_hours - 40
+    employee_over = employee_hours - 40
 
 # Caculations for how much a employee would get paid for normal hours, overtime hours, and thier gross pay.
 employee_pay_rate = employee_hours * employee_pay_rate
@@ -25,6 +26,5 @@ print("-----------------------------------------------------------------"*2);
 print(f"{'Employee name:':<30} {employee_name}");
 print();
 print(f"{'Hours Worked':<31} {'Pay Rate':<31} {'Overtime':<31} {'OverTime Pay':<31} {'RegHour Pay':<31} {'Gross Pay':<31} ");
-print(f"{'employee_hours':<31} {'employee_pay_rate':<31} {'employee_hours':<31} {'employee_over_time_pay':<31} {'employee_hours':<31} {'employee_hours':<31}  {'employee_hours':<31}");
-#print(f"{'Average:':<34} {avg:,.2f}");
+print(f"{'employee_hours':<31} {'employee_pay_rate':<31} {'employee_over':<31} {'employee_over_time_pay':<31} {'employee_pay_rate':<31} {'employee_gross_pay':<31}");
 print("----------"*50);
