f39876bd8c5c24b35bbfc7e31cc9a1fbfd1ce608
AshCowne <159182934+AshCowne@users.noreply.github.com>
2024-10-27 23:33:12 -0400

    Update P3HW2_AshCowne.py

--- P3HW2_AshCowne.py
+++ P3HW2_AshCowne.py
@@ -17,14 +17,14 @@ if employee_hours > 40 :
     employee_over = employee_hours - 40
 
 # Caculations for how much a employee would get paid for normal hours, overtime hours, and thier gross pay.
-employee_pay_rate = employee_hours * employee_pay_rate
-employee_over_time_pay = employee_over_time_pay * employee_pay_rate
-employee_gross_pay = employee_hours + employee_over_time_pay
+employee_pay_rate = (employee_hours * employee_pay_rate)
+employee_over_time_pay = (employee_over_time_pay * employee_pay_rate)
+employee_gross_pay = (employee_hours + employee_over_time_pay)
 
 # Displaying employee infomation.
 print("-----------------------------------------------------------------"*2);
 print(f"{'Employee name:':<30} {employee_name}");
 print();
 print(f"{'Hours Worked':<31} {'Pay Rate':<31} {'Overtime':<31} {'OverTime Pay':<31} {'RegHour Pay':<31} {'Gross Pay':<31} ");
-print(f"{'employee_hours':<31} {'employee_pay_rate':<31} {'employee_over':<31} {'employee_over_time_pay':<31} {'employee_pay_rate':<31} {'employee_gross_pay':<31}");
+print(f"{employee_hours:<31} {employee_pay_rate:<31} {employee_over:<31} {employee_over_time_pay:<31} {employee_pay_rate:<31} {employee_gross_pay:<31}");
 print("-------"*40);
