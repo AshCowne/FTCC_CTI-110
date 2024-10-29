66f8e13b32ab5fc9b1c6493bd0b5017bc0a545af
AshCowne <159182934+AshCowne@users.noreply.github.com>
2024-10-29 13:45:53 -0400

    Update P3HW2_AshCowne.py

--- P3HW2_AshCowne.py
+++ P3HW2_AshCowne.py
@@ -3,6 +3,7 @@
 # P3HW2
 
 # Assignment assess student understanding of decision structures.
+# Program to caculate a paychech based on overtime or no overtime.
 
 # Creating varables and asking input from user.
 employee_name = input("Please enter your name: ");
@@ -10,6 +11,7 @@ employee_hours = float(input("Please enter how many hours you've worked this wee
 employee_pay_rate = float(input("Please enter your pay rate (Specifically how much you make a hour): "));
 employee_over = 0
 employee_over_time_pay = 0
+employee_noramal_pay = 0
 employee_gross_pay = 0
 
 # if statment to chech if employee has overtime and caculates the extra hours of overtime.
@@ -17,14 +19,14 @@ if employee_hours > 40 :
     employee_over = employee_hours - 40
 
 # Caculations for how much a employee would get paid for normal hours, overtime hours, and thier gross pay.
-employee_pay_rate = (employee_hours * employee_pay_rate)
-employee_over_time_pay = (employee_over_time_pay * employee_pay_rate)
-employee_gross_pay = (employee_hours + employee_over_time_pay)
+employee_noramal_pay = (employee_hours * employee_pay_rate)
+employee_over_time_pay = (employee_over * employee_pay_rate)
+employee_gross_pay = (employee_noramal_pay + employee_over_time_pay)
 
 # Displaying employee infomation.
 print("-----------------------------------------------------------------"*2);
 print(f"{'Employee name:':<30} {employee_name}");
 print();
 print(f"{'Hours Worked':<31} {'Pay Rate':<31} {'Overtime':<31} {'OverTime Pay':<31} {'RegHour Pay':<31} {'Gross Pay':<31} ");
-print(f"{employee_hours:<31} {employee_pay_rate:<31} {employee_over:<31} {employee_over_time_pay:<31} {employee_pay_rate:<31} {employee_gross_pay:<31}");
+print(f"{employee_hours:<42} {employee_pay_rate:<36} {employee_over:<42} {employee_over_time_pay:<42} {employee_noramal_pay:<42} {employee_gross_pay:<42}");
 print("-------"*40);
