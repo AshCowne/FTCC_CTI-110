210f0f6bd5978f3402794db37e0cc17c19b721d8
AshCowne <159182934+AshCowne@users.noreply.github.com>
2024-12-14 01:38:00 -0500

    Add files via upload

---
+++ loop exaples.py
@@ -0,0 +1,81 @@
+#Ash Cowne
+#11/7/2024
+
+#for loop, rub predetermmed number of items two examples rangr() or list.
+# The parameter is the stop value, the stop value is NOT inclusive.
+
+# For loop with range with one parameter.
+#for var in range(10):
+    #print(var + 1);
+    #print("Apples are delicious!');)
+
+
+# For loop with range with two parameters.
+#Parameters are the starting value and ending value, ending not inclusive.
+#end_num = 14
+#for item in range(7,end_num+1):
+   # print(item);
+#
+# For loop with range with three parameters.
+# Paramaters are start, stop, not inclusive, number to increase by.
+#for cat in range(0, 101,5):
+  #  print(cat);
+#
+#
+# Looping a list.
+#Loop throw a list and print the items.
+#myList = ["one", "two", 2, 2.5, "THREE", 4, 4.5, "fIve", False]
+#
+#for thing in myList:
+ #  print(thing);
+#
+#numList = [10, 20, 30, 40, 50]
+#total = 0
+#
+#for num in numList:
+ #   total = total + num
+#
+#print(total);
+#
+#
+###############################################################
+
+# While loop examples.
+# while loops run till a certain condition is met and use a varable to control the loop, usally using <, >, <=, >=, ==, or !=
+#
+#run_again = input("Enter 'run' to run the program or 'exit' to end: ")
+#while run_again != "extit":
+#    print("Something..................................... Nothing?");
+#    run_again = input("Enter nothing to run again and see if somehting interseesting happens or enter exit to cancel program: ");
+#Loop breaks here.
+#print("Sorry that something didn't happen");
+#
+#While loop to run 5 times
+num = 0
+while num < 5:
+    print("\U0001f600 \U0001F606 \U0001F923 \N{grinning face} \N{slightly smiling face} \N{winking face} \U0001F601")
+    num += 1
+    
+
+#
+###
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
