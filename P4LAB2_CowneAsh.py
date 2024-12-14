707f70915ec76fce6349be94159660ae2c98b0dd
AshCowne <159182934+AshCowne@users.noreply.github.com>
2024-12-14 01:35:13 -0500

    Add files via upload

---
+++ P4LAB2_CowneAsh.py
@@ -0,0 +1,22 @@
+# Ash Cowne
+# 11/7/2024
+# P4LAB2
+# Use loops to show only positive time tables.
+run_again = "yes"
+
+
+while run_again.lower() != "no":
+    print("Show times table if user enters positive interger.");
+
+    #Get an integer from the user.
+    # if the integer is a negative dont run and start the program again.
+    # if integer is positive show the times table.
+    # Use a for loop to show the times table.
+
+
+
+
+
+    run_again = input("Would you like to run the program again? enter yes or no: ")
+    
+print("Program has ended.")
