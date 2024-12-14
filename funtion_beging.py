129bcf5d805bb5d131ce8811b820751495107430
AshCowne <159182934+AshCowne@users.noreply.github.com>
2024-12-14 01:38:22 -0500

    Add files via upload

---
+++ funtion_beging.py
@@ -0,0 +1,21 @@
+# Beging funtion
+
+# Difine a funtion that adds numbers.
+def add_num(num1, num2, num3):
+    result = num1 + num2 + num3
+    print(f"***{result}***");
+
+
+def main():
+    #Mail logic goes here
+    print("The main is running if you see this start running your losing time.");
+    # Callt the add_num function.
+    add_num(3,12,23)
+    add_num("ffire465487uurty","ewrrtysrthgfnsfghdsgadfga","hwaweal[fsapdf[90f")
+    add_num(0,0,0)
+    print("To late.");
+
+
+# Calls main
+if __name__ == "__main__":
+    main()
